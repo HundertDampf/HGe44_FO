3dd2817a1f49032ae45b1ed3ea55029f0c4ce7d1
HundertDampf <100dampf@gmail.com>
2018-11-04 21:34:33 +0100

    104 FO erstellt

---
+++ hundertdampf_hge44IIfo_1/mod.lua
@@ -0,0 +1,31 @@
+﻿function data()
+return {
+  info = {
+  minorVersion = 0,
+  severityAdd = "NONE",
+  severityRemove = "WARNING",
+  name = _("HGe 4/4 II der FO"),
+  description = _("Die HGe 4/4 II ist eine elektrische Meterspurlokomotive. Sie besitzt einen Zahnradantrieb und wird vor schweren Personen- und Güterzügen eingesetzt.   \n" .. 
+				"Enthalten ist die Variante der Matterhorn Gotthard Bahn."),
+  authors = {
+	{
+	name = "Skelis",
+	 role = "CREATOR",
+	 text = "Model, Textures",
+	 tfnetId = 21482,
+    },
+  },
+  tags = {"vehicle", "train"},
+  tfnetId = {},
+  minGameVersion = {},
+  dependencies = {},
+  url = {""},
+},
+		options = {
+		},
+	
+		runFn = function (settings)
+		
+		end
+	}
+end
